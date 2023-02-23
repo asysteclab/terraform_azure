2a9212ee758daad40be6612d36ff93e68ae80fc9
asysteclab <ciaran.cooney@asytec.ie>
2023-02-23 11:00:54 +0000

    refactored to single resource group

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
   }
-
   required_version = ">= 1.1.0"
 }
 
@@ -13,33 +12,28 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "azure_vm_resource_group" {
-  name     = "vRADemoResourceGroup"
-  location = "northeurope"
-}
-
-resource "azurerm_resource_group" "azure_image_gallery_resource_group" {
-  name     = "packerImageGallery"
+resource "azurerm_resource_group" "azure_daas_rg" {
+  name = "Azure-Daas-RG"
   location = "northeurope"
 }
 
 resource "azurerm_shared_image_gallery" "azure_image_gallery" {
-  name                = "packerImageGallery"
-  resource_group_name = azurerm_resource_group.azure_image_gallery_resource_group.name
-  location            = "northeurope"
+  name = "packerImageGallery"
+  resource_group_name = azurerm_resource_group.azure_daas_rg.name
+  location = "northeurope"
 }
 
 resource "azurerm_shared_image" "windows10_shared_image" {
-  name                = "windows10-image-azure"
-  gallery_name        = azurerm_shared_image_gallery.azure_image_gallery.name
-  resource_group_name = azurerm_resource_group.azure_image_gallery_resource_group.name
-  location            = "northeurope"
-  os_type             = "Windows"
-  hyper_v_generation  = "V2"
+  name = "windows10-image-azure"
+  gallery_name = azurerm_shared_image_gallery.azure_image_gallery.name
+  resource_group_name = azurerm_resource_group.azure_daas_rg.name
+  location = "northeurope"
+  os_type = "Windows"
+  hyper_v_generation = "V2"
 
   identifier {
     publisher = "AIB"
-    offer     = "Windows-10"
-    sku       = "windows10-image-azure"
+    offer = "Windows-10"
+    sku = "windows10-image-azure"
   }
 }
